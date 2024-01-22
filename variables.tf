4b03d90661ce269e6b12f0b2f350f47098d2dcc7
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-22 18:48:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
 variable "AWS_ACCESS_KEY" {}
 
 variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "aws_region" {
+  description = "Region in which AWS resource is to be created"
+  type = string
+  default = "us-east-1"
+
+variable 'instance_type" {
+  description = "EC2 instance type"
+  type = string
+  default "t3.micro"
+}
