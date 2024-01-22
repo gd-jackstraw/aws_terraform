b5da675e826efac6f43ea882d0c459bddb03995c
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-22 18:56:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "awsvpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws"s3"bucket" "bucket_1" {
+resource "aws_s3_bucket" "bucket_1" {
   count = 2
   tags = {
     name="test-bucket-${count.index}"
