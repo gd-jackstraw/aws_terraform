4ee34bb2ebd804b8276b9d53107959f04fbb175f
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-22 18:48:37 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
   type = string
   default = "us-east-1"
 
-variable 'instance_type" {
+variable "instance_type" {
   description = "EC2 instance type"
   type = string
   default "t3.micro"
