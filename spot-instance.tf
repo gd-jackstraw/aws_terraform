a393cea304498c0191f5d20d2a700bf305777857
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-22 20:43:50 -0500

    Update spot-instance.tf

--- spot-instance.tf
+++ spot-instance.tf
@@ -1,6 +1,7 @@
 # Request a spot instance at $0.03
 resource "aws_spot_instance_request" "cheap_instance" {
   count = 3
+  name = "cheap-worker-${count.index}"
   ami           = data.aws_ami.amz_linux2.id
   spot_price    = "0.03"
   instance_type = var.instance_type
