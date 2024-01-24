d9f54ab738a798562f7a8fe594dfecac6954da48
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-24 18:27:06 -0500

    Create vars.tf

---
+++ terraform-aws-modules/vpc/aws/module_vpc/vars.tf
@@ -0,0 +1,11 @@
+variable "vpc_availability_zones" {}
+variable "vpc_public_subnets" {}
+variable "vpc_private_subnets  " {}
+variable "vpc_database_subnets" {}
+variable "vpc_create_database_subnet_group" {}
+variable "vpc_create_database_subnet_route_table" {}
+variable "vpc_create_database_subnet_group" {}
+variable "vpc_enable_nat_gateway" {}
+variable "vpc_single_nat_gateway" {}
+
+locals {common_tags = "dns-param"}
