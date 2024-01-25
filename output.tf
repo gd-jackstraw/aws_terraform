ae9dd1ad3f2bd0697b44a261f6c414b90f048679
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-24 20:58:29 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,22 @@
+
+output "vpc_cidr_block_output" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr
+}
+
+output "private_subnets_output" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets_output" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+
+output "azs_output" {
+  description = "A list of availability zones spefified as argument to this module"
+  value       = module.vpc.azs
+}
+
