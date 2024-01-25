06f2e37c743d47963a0b935f171843f5742eacfb
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-24 21:05:35 -0500

    Update variable-module.tf

--- variable-module.tf
+++ variable-module.tf
@@ -4,7 +4,7 @@
   default     = "assignment-vpc"
 }
 
-variable "vpc_cidr" {
+variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
