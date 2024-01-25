859ae3b74e83209de8d73025f04ce705b4aa15f8
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-24 19:21:52 -0500

    Update output.tf

--- terraform-aws-modules/vpc/aws/output.tf
+++ terraform-aws-modules/vpc/aws/output.tf
@@ -1,6 +1,6 @@
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = module.vpc.vpc_cidr_block
+  value       = module.vpc.cidr
 }
 
 output "private_subnets" {
@@ -13,10 +13,6 @@ output "public_subnets" {
   value       = module.vpc.public_subnets
 }
 
-output "nat_public_ips" {
-  description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.vpc.nat_public_ips
-}
 
 output "azs" {
   description = "A list of availability zones spefified as argument to this module"
